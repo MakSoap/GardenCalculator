4894db4cf708c6c7828a9ec01ce84cbbc7ad18a9
MakSoap <cool.anton1@yandex.ru>
2022-04-22 20:48:25 +0300

    Добавлен класс констант

---
+++ src/main/kotlin/ru/maksoap/core/constants/Constants.kt
@@ -0,0 +1,50 @@
+package ru.maksoap.core.constants
+
+
+object Constants {
+
+   const val U: Long = 648648000
+   const val U2 = U / 2
+   const val U3 = U / 3
+   const val U4 = U / 4
+   const val U5 = U / 5
+   const val U6 = U / 6
+   const val U7 = U / 7
+   const val U8 = U / 8
+   const val U9 = U / 9
+   const val U10 = U / 10
+   const val U11 = U / 11
+   const val U12 = U / 12
+   const val U13 = U / 13
+   const val U14 = U / 14
+   const val U15 = U / 15
+   const val U16 = U / 16
+   const val U17 = U / 17
+   const val U18 = U / 18
+   const val U20 = U / 20
+   const val U24 = U / 24
+   const val U25 = U / 25
+   const val U32 = U / 32
+   const val U36 = U / 36
+   const val U40 = U / 40
+   const val U48 = U / 48
+   const val U50 = U / 50
+   const val U64 = U / 64
+   const val U72 = U / 72
+   const val U80 = U / 80
+   const val U96 = U / 96
+   const val U100 = U / 100
+   const val U128 = U / 128
+   const val U144 = U / 144
+   const val U192 = U / 192
+   const val U200 = U / 200
+   const val U240 = U / 240
+   const val U256 = U / 256
+   const val U288 = U / 288
+   const val U480 = U / 480
+   const val U500 = U / 500
+   const val U512 = U / 512
+   const val U1000 = U / 1000
+   const val U1440 = U / 1440
+
+}
