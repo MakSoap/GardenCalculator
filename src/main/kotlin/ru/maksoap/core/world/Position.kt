dad7f2c7f9f5616c892b19a4eeba21eeb17ad285
MakSoap <cool.anton1@yandex.ru>
2022-04-22 22:07:19 +0300

    Добавлен класс позиции

---
+++ src/main/kotlin/ru/maksoap/core/world/Position.kt
@@ -0,0 +1,25 @@
+package ru.maksoap.core.world
+
+data class Position(var x: Int, var y: Int) {
+
+    fun up(): Position          = Position(x, y + 1)
+    fun upLeft(): Position      = Position(x - 1, y + 1)
+    fun upRight(): Position     = Position(x + 1, y + 1)
+    fun back(): Position        = Position(x, y - 1)
+    fun backLeft(): Position    = Position(x - 1, y - 1)
+    fun backRight(): Position   = Position(x + 1, y - 1)
+    fun left(): Position        = Position(x - 1, y)
+    fun right(): Position       = Position(x + 1, y)
+
+    fun setPosition(_x: Int, _y: Int) {
+        x = _x
+        y = _y
+    }
+
+    fun setPosition(pos: Position) {
+        x = pos.x
+        y = pos.y
+    }
+
+    fun copy() = Position(x, y)
+}
